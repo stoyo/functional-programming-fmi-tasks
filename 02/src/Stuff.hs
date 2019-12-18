9f13179a36ae95a1060a315c47a7a2f8bce5ddbf
stoyo <genchev_stoyan@abv.bg>
2019-12-19 00:26:56 +0200

    add solutions to third homework

--- 02/src/Stuff.hs
+++ 02/src/Stuff.hs
@@ -9,30 +9,39 @@ module Stuff
   , on
   ) where
 
-group :: Eq a => [a] -> [[a]]
-group = undefined
+import Data.List (partition)
 
--- Not mandatory, delete if you don't want this.
-insertBy :: (a -> a -> Ordering) -> a -> [a] -> [a]
-insertBy = undefined
+group :: Eq a => [a] -> [[a]]
+group [] = []
+group xs = go xs []
+ where
+   go [] grouped = grouped
+   go (y:ys) grouped = go (dropWhile (==y) ys) (grouped ++ [y : takeWhile (==y) ys])
 
 sortBy :: (a -> a -> Ordering) -> [a] -> [a]
-sortBy = undefined
+sortBy _ [] = []
+sortBy comparator (pivot:others) = sortBy comparator lessThanOrEqual ++ (pivot : sortBy comparator greater)
+ where
+   (lessThanOrEqual, greater) = partition (\el -> comparator el pivot /= GT) others
 
 groupBy :: (a -> a -> Bool) -> [a] -> [[a]]
-groupBy = undefined
+groupBy _ [] = []
+groupBy f xs = go f xs []
+ where
+   go _ [] grouped = grouped
+   go g (y:ys) grouped = go g (dropWhile (g y) ys) (grouped ++ [y : takeWhile (g y) ys])
 
 on :: (b -> b -> c) -> (a -> b) -> a -> a -> c
-on = undefined
+on b u x y = b (u x) (u y)
 
 (&&&) :: (a -> b) -> (a -> c) -> a -> (b, c)
-(&&&) = undefined
+(&&&) toB toC a = (toB a, toC a)
 
 sortOn :: Ord b => (a -> b) -> [a] -> [a]
-sortOn = undefined
+sortOn f = sortBy (compare `on` f)
 
 groupOn :: Eq b => (a -> b) -> [a] -> [[a]]
-groupOn = undefined
+groupOn f = groupBy ((==) `on` f)
 
 classifyOn :: Ord b => (a -> b) -> [a] -> [[a]]
-classifyOn = undefined
+classifyOn f xs = groupOn f (sortOn f xs)
