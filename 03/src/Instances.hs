6cf2ea697c4e86f8e94cf83ad6d8f6069e9ebfbe
stoyo <genchev_stoyan@abv.bg>
2020-01-09 21:50:00 +0200

    add solutions to fourth homework

--- 03/src/Instances.hs
+++ 03/src/Instances.hs
@@ -13,7 +13,7 @@ newtype Pointwise a b = Pointwise {getPointwise :: (a, b)}
 
 instance (Ord a, Ord b) => Ord (Pointwise a b) where
   (<=) :: Pointwise a b -> Pointwise a b -> Bool
-  (<=) = undefined
+  Pointwise (p1a, p1b) <= Pointwise (p2a, p2b) = p1a <= p2a && p1b <= p2b
 
 newtype Lexicographic a b = Lexicographic {getLexicographic :: (a, b)}
   deriving (Show, Eq)
@@ -21,39 +21,41 @@ newtype Lexicographic a b = Lexicographic {getLexicographic :: (a, b)}
 -- The default instance for tuples and lists
 instance (Ord a, Ord b) => Ord (Lexicographic a b) where
   (<=) :: Lexicographic a b -> Lexicographic a b -> Bool
-  (<=) = undefined
+  Lexicographic (l1a, l1b) <= Lexicographic (l2a, l2b) = l1a < l2a || (l1a == l2a && l1b <= l2b)
 
 newtype Fun a b = Fun {getFun :: a -> b}
 
 instance (Semigroup b) => Semigroup (Fun a b) where
   (<>) :: Fun a b -> Fun a b -> Fun a b
-  (<>) = undefined
+  (Fun f) <> (Fun g) = Fun (\x -> f x <> g x)
 
 instance (Monoid b) => Monoid (Fun a b) where
   mempty :: Fun a b
-  mempty = undefined
+  mempty = Fun mempty
 
 newtype First a = First {getFirst :: Maybe a}
   deriving (Eq, Show)
 
 instance Semigroup (First a) where
   (<>) :: First a -> First a -> First a
-  (<>) = undefined
+  First Nothing <> b = b
+  a <> _ = a
 
 instance Monoid (First a) where
   mempty :: First a
-  mempty = undefined
+  mempty = First Nothing
 
 newtype Last a = Last {getLast :: Maybe a}
   deriving (Eq, Show)
 
 instance Semigroup (Last a) where
   (<>) :: Last a -> Last a -> Last a
-  (<>) = undefined
+  a <> Last Nothing = a
+  _ <> b = b
 
 instance Monoid (Last a) where
   mempty :: Last a
-  mempty = undefined
+  mempty = Last Nothing
 
 newtype Pair a b = Pair {getPair :: (a, b)}
   deriving (Show, Eq)
@@ -61,25 +63,25 @@ newtype Pair a b = Pair {getPair :: (a, b)}
 -- The default instance for tuples
 instance (Semigroup a, Semigroup b) => Semigroup (Pair a b) where
   (<>) :: Pair a b -> Pair a b -> Pair a b
-  (<>) = undefined
+  Pair (p1a, p1b) <> Pair (p2a, p2b) = Pair (p1a <> p2a, p1b <> p2b)
 
 instance (Monoid a, Monoid b) => Monoid (Pair a b) where
   mempty :: Pair a b
-  mempty = undefined
+  mempty = Pair (mempty, mempty)
 
 newtype Dual a = Dual {getDual :: a}
   deriving (Show, Eq)
 
 instance Semigroup a => Semigroup (Dual a) where
   (<>) :: Dual a -> Dual a -> Dual a
-  (<>) = undefined
+  Dual a <> Dual b = Dual (b <> a)
 
 instance Monoid a => Monoid (Dual a) where
   mempty :: Dual a
-  mempty = undefined
+  mempty = Dual mempty
 
 reverse :: [a] -> [a]
-reverse = undefined
+reverse = foldr (\x xs -> getDual (Dual [x] <> Dual xs)) [] -- couldn't make it worse :D
 
 data Flux a = Flux
   { sides :: Maybe (a, a)
@@ -92,8 +94,11 @@ flux x = Flux (Just (x, x)) 0
 
 instance (Eq a) => Semigroup (Flux a) where
   (<>) :: Flux a -> Flux a -> Flux a
-  (<>) = undefined
+  Flux Nothing _ <> g = g
+  f <> Flux Nothing _ = f
+  Flux (Just (j1a, j1b)) j1n <> Flux (Just (j2a, j2b)) j2n =
+    Flux (Just (j1a, j2b)) (j1n + j2n + (if j1b /= j2a then 1 else 0))
 
 instance (Eq a) => Monoid (Flux a) where
   mempty :: Flux a
-  mempty = undefined
+  mempty = Flux Nothing 0
--- 03/src/Trees.hs
+++ 03/src/Trees.hs
@@ -14,63 +14,82 @@ data Tree a
 
 instance Eq a => Eq (Tree a) where
   (==) :: Tree a -> Tree a -> Bool
-  (==) = undefined
+  Empty == Empty = True
+  Empty == _ = False
+  _ == Empty = False
+  (Node t1Val t1Left t1Right) == (Node t2Val t2Left t2Right) = t1Left == t2Left && t1Val == t2Val && t1Right == t2Right
 
 insertOrdered :: Ord a => a -> Tree a -> Tree a
-insertOrdered = undefined
+insertOrdered newVal Empty = Node newVal Empty Empty
+insertOrdered newVal (Node rootVal t1 t2)
+  | newVal > rootVal = Node rootVal t1 (insertOrdered newVal t2)
+  | otherwise = Node rootVal (insertOrdered newVal t1) t2
 
 listToBST :: Ord a => [a] -> Tree a
-listToBST = undefined
+listToBST xs = go xs Empty
+  where
+    go ys tree = foldl (flip insertOrdered) tree ys
 
 isBST :: Ord a => Tree a -> Bool
-isBST = undefined
-
--- idea for implementing isBST - delete if you don't want it
-data BotTop a = Bot | Val a | Top
-  deriving (Show, Eq, Ord)
-
-between :: Ord a => BotTop a -> BotTop a -> Tree a -> Bool
-between = undefined
+isBST Empty = True
+isBST (Node _ Empty Empty) = True
+isBST (Node val left@(Node leftVal _ _) Empty)
+  | val >= leftVal = isBST left
+  | otherwise = False
+isBST (Node val Empty right@(Node rightVal _ _))
+  | val < rightVal = isBST right
+  | otherwise = False
+isBST (Node val left@(Node leftVal _ _) right@(Node rightVal _ _))
+  | val >= leftVal && val < rightVal = isBST left && isBST right
+  | otherwise = False
 
 findBST :: Ord a => a -> Tree a -> Bool
-findBST = undefined
+findBST _ Empty = False
+findBST target (Node root left right)
+  | target == root = True
+  | target < root = findBST target left
+  | otherwise = findBST target right
 
 mapTree :: (a -> b) -> Tree a -> Tree b
-mapTree = undefined
+mapTree _ Empty = Empty
+mapTree f (Node val left right) = Node (f val) (mapTree f left) (mapTree f right)
 
 foldTree :: Monoid a => Tree a -> a
-foldTree = undefined
+foldTree Empty = mempty
+foldTree (Node val left right) = foldTree left <> val <> foldTree right
 
 foldMapTree :: Monoid b => (a -> b) -> Tree a -> b
-foldMapTree = undefined
+foldMapTree f = foldTree . mapTree f
 
 sumTree :: Num a => Tree a -> a
-sumTree = undefined
+sumTree = getSum . foldMapTree Sum
 
 allTree :: (a -> Bool) -> Tree a -> Bool
-allTree = undefined
+allTree p = getAll . foldMapTree (All . p)
 
 treeToList :: Tree a -> [a]
-treeToList = undefined
+treeToList = foldMapTree (: [])
 
 elemTree :: Eq a => a -> Tree a -> Bool
-elemTree = undefined
+elemTree val = getAny . foldMapTree (Any . (== val))
 
 onMaybe :: (a -> Bool) -> a -> Maybe a
 onMaybe p x = if p x then Just x else Nothing
 
 findPred :: (a -> Bool) -> Tree a -> Maybe a
-findPred = undefined
+findPred p tree = getFirst (foldMapTree (First . onMaybe p) tree)
 
 findAll :: (a -> Bool) -> Tree a -> [a]
-findAll = undefined
+findAll p = foldMapTree (\a -> [a | p a])
 
 ifJust :: Maybe a -> (a -> Maybe b) -> Maybe b
 ifJust Nothing _ = Nothing
 ifJust (Just x) f = f x
 
 validateTree :: (a -> Maybe b) -> Tree a -> Maybe (Tree b)
-validateTree = undefined
+validateTree _ Empty = Just Empty
+validateTree f (Node x l r) =
+  ifJust (validateTree f l) (\l' -> ifJust (f x) (\x' -> ifJust (validateTree f r) (Just . Node x' l')))
 
 data Direction
   = L -- go left
@@ -78,7 +97,23 @@ data Direction
   deriving (Show, Eq)
 
 fetch :: [Direction] -> Tree a -> Maybe a
-fetch = undefined
+fetch [] Empty = Nothing
+fetch [] (Node val _ _) = Just val
+fetch (_:_) Empty = Nothing
+fetch (dir:dirs) (Node _ left right) =
+  case dir of
+    L -> fetch dirs left
+    R -> fetch dirs right
+
+mapDirections :: Tree a -> Tree (a, [Direction])
+mapDirections Empty = Empty
+mapDirections (Node v l r) = Node (v, []) (go [] L l) (go [] R r)
+  where
+    go :: [Direction] -> Direction -> Tree a -> Tree (a, [Direction])
+    go _ _ Empty = Empty
+    go dirs dir (Node value left right) =
+      let dirsOnLevel = dirs ++ [dir]
+      in Node (value, dirsOnLevel) (go dirsOnLevel L left) (go dirsOnLevel R right)
 
 paths :: Tree a -> [(a, [Direction])]
-paths = undefined
+paths = treeToList . mapDirections
